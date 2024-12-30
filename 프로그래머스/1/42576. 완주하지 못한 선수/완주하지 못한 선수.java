a69e19e210ebbe3153e2b043a713db766bb0043c
kiwi1020 <108050749+kiwi1020@users.noreply.github.com>
2024-12-30 15:32:41 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 351.05 ms, Memory: 96.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,13 +1,12 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        
         Arrays.sort(participant);
         Arrays.sort(completion);
-
-        for (int i = 0; i < completion.length; i++) {
-            if (!participant[i].equals(completion[i]))
+        
+        for (int i = 0; i < completion.length; i++) 
+              if (!participant[i].equals(completion[i])){
                 return participant[i];
         }
         return participant[participant.length - 1];
