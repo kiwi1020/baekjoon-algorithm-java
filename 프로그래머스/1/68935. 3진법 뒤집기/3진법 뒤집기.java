69fd18dad993d766b726b3187b03a181c66872bb
Wooyeol - YOO <108050749+kiwi1020@users.noreply.github.com>
2025-05-02 21:39:38 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 89.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n) {
+        StringBuilder ternary = new StringBuilder();
+        while (n > 0) {
+            ternary.append(n % 3);
+            n /= 3;
+        }
+        ternary.reverse();
+
+        int answer = 0;
+        int power = 1;
+        for (int i = 0; i < ternary.length(); i++) {
+            answer += (ternary.charAt(i) - '0') * power;
+            power *= 3;
+        }
+
+        return answer;
+    }
+}
